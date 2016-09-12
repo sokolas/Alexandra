ed27de45250aacec16a69903c2163ab99910e0b8
Andrey.Sokolov <Andrey.Sokolov@billing.ru>
2016-09-12 19:32:08 +0300

    I dunno

--- src/main/java/persistence/PlayerDbo.java
+++ src/main/java/persistence/PlayerDbo.java
@@ -1,5 +1,20 @@
 package persistence;
 
-public class PlayerDbo {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class PlayerDbo {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/rpgbot/Player.java
+++ src/main/java/rpgbot/Player.java
@@ -2,10 +2,53 @@ package rpgbot;
 
 import sx.blah.discord.handle.obj.IUser;
 
+
 public class Player {
 	private IUser user;
+
+    private Long id;
 
 	private int attack;
 	private int hp;
 	private int damage;
+
+    public IUser getUser() {
+        return user;
+    }
+
+    public void setUser(IUser user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
 }
